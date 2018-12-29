ea08c70cabafd3e2b79d64676816035eaf966b6d
Bar Horing <bar.apple@gmail.com>
2018-12-29 15:12:36 +0200

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,12 +1,33 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+class Square extends React.Component {
+  render() {
+    return <button className="square">{/* todo */}</button>;
+  }
+}
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+class Game extends React.Component {
+  render() {
+    return (
+      <div className="game">
+        <div className="game-board">
+          <Square />
+        </div>
+        <div className="game-info">
+          <div> {/* status */}</div>
+          <ol>{/* TODO */}</ol>
+        </div>
+      </div>
+    );
+  }
+}
+
+// =======================================================
+
+ReactDOM.render(<Game />, document.getElementById("root"));
+
+// game - board;
+
+// game - info;
