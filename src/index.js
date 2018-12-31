80699e12e695720f853ec773711a04dfcacdfc43
Bar Horing <bar.apple@gmail.com>
2018-12-31 21:53:31 +0200

    going to sleep

--- src/index.js
+++ src/index.js
@@ -24,6 +24,7 @@ class Board extends React.Component {
     const xIsNext = this.state.xIsNext;
     squares[i] = xIsNext ? "X" : "O";
     this.setState({ squares: squares, xIsNext: !xIsNext });
+    calculateWinner(this.state.squares);
   };
 
   renderSquare(i) {
@@ -78,3 +79,21 @@ class Game extends React.Component {
 // =======================================================
 
 ReactDOM.render(<Game />, document.getElementById("root"));
+
+function calculateWinner(squares) {
+  const winnerLines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+  ];
+  for (let i = 0; i < winnerLines.length; i++) {
+    const line = winnerLines[i];
+    console.log(line);
+  }
+  return null;
+}
